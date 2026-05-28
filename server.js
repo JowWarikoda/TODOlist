f2a64d5e12d1c239415fce134bc09adcaf839592
Jow Warikoda <jowarikoda@gmail.com>
2026-05-27 22:25:30 -0300

    refactor: integrating postgresql database

--- server.js
+++ server.js
@@ -1,60 +1,129 @@
 import express from "express";
+import knex from "./db/knex.js";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-let tasksToDo = [];
-
 // Listar TODOs, tarefas para o usuario
-app.get("/TODOs", (req, res) => {
-  res.json(tasksToDo);
+app.get("/todos", async (req, res) => {
+  try {
+    await knex
+      .select()
+      .from("taskstodo")
+      .then(function (taskstodo) {
+        res.status(200).json(taskstodo);
+      }); // Pesquisar a diff entre .json .send
+
+    /* Raw Version, kinda longer
+    await knex.raw("SELECT * from taskstodo").then(function (tasksToDo) {
+      res.status(200).send(tasksToDo.rows);
+    }); */
+  } catch (e) {
+    res.status(400).json({
+      e: e.message,
+    });
+  }
 });
 
-let id = -1;
-
 // CREATE / POST
-app.post("/TODOs", (req, res) => {
+app.post("/todos", async (req, res) => {
   try {
-    id = ++id;
-    const title = req.body.title;
-    const isDone = false;
-    const newTaskToDo = {
-      id,
-      title,
-      isDone,
-    };
-    tasksToDo.push(newTaskToDo);
-    res.json(newTaskToDo);
-  } catch (error) {
-    res.json({
-      error: error.message,
+    await knex("taskstodo")
+      .insert({
+        title: req.body.title,
+      })
+      .then(function () {
+        knex
+          .select()
+          .from("taskstodo")
+          .then(function (taskstodo) {
+            res
+              .status(201)
+              .json({ message: "New Task created with Success", taskstodo });
+          });
+      });
+    /* Raw Version
+    await knex.raw("INSERT into taskstodo (title) values (?)", ["NEW TASK CREATED 1"]).then(function () {
+        knex.select().from("taskstodo").then(function (newtasktodo) {res.status(201).send(newtasktodo);
+          }); 
+      }); */
+  } catch (e) {
+    if (e.code === "22001") {
+      // String Data Right Truncation -> Code (22001)
+      return res.status(400).json({
+        message:
+          "Title exceeds character limit. Title can only have up to maximum 200 characters",
+      });
+    }
+
+    // Validação para impedir a criação de uma task vazia sem caractere
+    res.status(400).json({
+      e: e.message,
     });
   }
 });
 
 // UPDATE / PUT
-app.put("/TODOs/:id", (req, res) => {
+app.put("/todos/:id", async (req, res) => {
   try {
-    const newTaskToDo = tasksToDo.find((task) => req.params.id == task.id);
-    newTaskToDo.title = req.body.title;
-    res.json(newTaskToDo);
-  } catch (error) {
-    res.json({
-      error: error.message,
+    const updatedRows = await knex("taskstodo")
+      .where("id", req.params.id)
+      .update({
+        title: req.body.title,
+        isDone: req.body.isDone,
+      });
+    if (updatedRows === 0) {
+      res.status(404).json({
+        message: "Task not found",
+      });
+    }
+    res.status(200).json({
+      message: `The task ${req.params.id} was updated with Success`,
+    });
+    /*Raw Version
+    knex.raw('UPDATE taskstodo SET ' + req.body.field + " = ? where id = ?", [req.body.value, reqparams.id])
+      res
+        .status(200)
+        .json({ message: "Task updated with Success!", newTaskToDo }); */
+  } catch (e) {
+    if (e.code === "22001") {
+      return res.status(400).json({
+        message:
+          "Title exceeds character limit. Title can only have up to maximum 200 characters",
+      });
+    }
+    res.status(500).json({
+      error: e.message,
     });
   }
 });
 
 // DELETE
-app.delete("/TODOs/:id", (req, res) => {
+app.delete("/todos/:id", async (req, res) => {
   try {
-    tasksToDo = tasksToDo.filter((task) => req.params.id != task.indexTask);
-    res.json(tasksToDo);
-  } catch (error) {
-    res.json({
-      error: error.message,
+    const deletedRows = await knex("taskstodo")
+      .where("id", req.params.id)
+      .del();
+
+    if (deletedRows === 0) {
+      res.status(404).json({
+        message: "Task not found",
+      });
+    }
+    res.status(200).json({
+      //Certo usar 204
+      message: `Task ${req.params.id} deleted with Success!`,
+    });
+
+    /* Raw Version
+    app.delete('DELETE FROM taskstodo wher id = ?', req.params.id) */
+
+    // RF05.03 Adicionar confirmacao de exclusao, entrada de dado pedindo um Y/N if Y entra pra delete se nao, nao deleta
+  } catch (e) {
+    res.status(500).json({
+      e: e.message,
     });
   }
 });
